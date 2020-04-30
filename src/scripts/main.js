caa5b5eb2fbc0a8499046def19e09543723f4f77
jansenv <jansenv44@gmail.com>
2020-04-29 20:58:20 -0500

    fired pottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
+import { firePottery } from "./Kiln.js"
 
 
 // Make 5 pieces of pottery at the wheel
@@ -10,7 +11,11 @@ let bowl = makePottery("bowl", 6, 7);
 let cup = makePottery("cup", 1, 2);
 
 // Fire each piece of pottery in the kiln
-
+let fireMug = firePottery(mug, 2000);
+let firePot = firePottery(pot, 2500);
+let fireKettle = firePottery(kettle, 2200);
+let fireBowl = firePottery(bowl, 1800);
+let fireCup = firePottery(cup, 5000);
 
 // Determine which ones should be sold, and their price
 
