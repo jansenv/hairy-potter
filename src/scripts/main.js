1e114107df6c72817bbc4ce241f9fb71bd977804
jansenv <jansenv44@gmail.com>
2020-04-29 20:47:46 -0500

    made 5 pieces of pottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,13 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
+let mug = makePottery("mug-shaped", 3, 5);
+let pot = makePottery("circular", 3, 6);
+let kettle = makePottery("kettle-shaped", 1, 5);
+let bowl = makePottery("bowl", 6, 7);
+let cup = makePottery("cup", 1, 2);
 
 // Fire each piece of pottery in the kiln
 
